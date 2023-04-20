760f80e02d8d1d293d0619291b553d88c4f76670
MbiakuChiamaka <chiamakambiaku.a@gmail.com>
2023-04-20 16:18:00 -0700

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - C program that prints a line with printf
  * Return: Always 0
- */
+*/
 int main(void)
 {
-printf(" with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
