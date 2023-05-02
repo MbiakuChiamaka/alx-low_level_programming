5c9347274addd4fdab8ac3beb6ffe96062ffee2d
MbiakuChiamaka <chiamakambiaku.a@gmail.com>
2023-05-02 08:02:04 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98-function thats takes the pointer and updates the value
+ * @n:int to check to 98
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
