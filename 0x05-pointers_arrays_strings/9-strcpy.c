f25de60a04e7649a0ed40b5632b5d648cf83f07b
MbiakuChiamaka <chiamakambiaku.a@gmail.com>
2023-05-02 19:20:40 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _strcpy-to copy the string that is pointed to by src to dest
- * @dest: char to be checked
- * @src: char to be checked
+ * @dest: char to check
+ * @src: char to check
  * Return: Always 0
  */
 char *_strcpy(char *dest, char *src)
 {
-	int m;
+	int a;
 
-	for (k = 0; src[k] != '\0'; k++)
-		dest[k] = src[k];
-	dest[k] = '\0';
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
 	return (dest);
 }
